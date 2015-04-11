784abf9ac14069ad6c6c2197373fc1e585bdda62
Larry Denneau <denneau@cadeo.ifa.hawaii.edu>
2015-04-10 20:32:54 -1000

    Comments!

--- scrape.pl
+++ scrape.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/env perl
 
+# Turn an HTML table from the JPL risk page into a CSV list of impact
+# solutions for a given asteroid.  There should not be a space in the 
+# name, e.g. 2015BR12.
+
+# This script is TOTALLY dependent on the format of the JPL HTML tables
+# on the risk page.
 $obj = shift @ARGV or die "Usage: $0 OBJECT\n";
 $lcobj = $obj;
 $lcobj =~ tr/A-Z/a-z/;
@@ -15,22 +21,25 @@ if (!$stuff) {
     die "No data for $obj using URL $url\n";
 }
 
-# sub the crap out of it.
-# 1. Remove all whitespace.
-$stuff =~ s/\s+//g;
-$stuff =~ s/^.*resultswerecomputed//;
-$stuff =~ s/SummaryTableDescription.*//;
+# Sub the heck out of it.
+$stuff =~ s/\s+//g;                         # remove ALL whitespace
+$stuff =~ s/^.*resultswerecomputed//;       # trim to approx beginning of table
+$stuff =~ s/SummaryTableDescription.*//;    # trim after and of table
 
+# Now break lines at <tr>
 @stuff = split /<\/tr>/, $stuff;
+
+# Accept only lines that have data.
 @stuff = grep { /<tt>\d\d\d\d-\d\d-\d\d/ or /<b>Date/ } @stuff;
 
+# Clean em up.
 foreach (@stuff) {
-    s/<td.*?>/\|/g;
-    s/<.*?>//g;
-    s/&nbsp;//g;
-    s/^\|/$me\|/;
-    s/\|/,/g;
+    s/<td.*?>/\|/g;     # remove table tags
+    s/<.*?>//g;         # remove all other tags
+    s/&nbsp;//g;        # remove &nbsp; (non-breaking space)
+    s/^\|/$me\|/;       # add leading object name
+    s/\|/,/g;           # to CSV
 }
-$stuff[0] =~ s/^$me/Object/;
+$stuff[0] =~ s/^$me/Object/;        # hack first line
 
-print join("\n", @stuff), "\n";
+print join("\n", @stuff), "\n";     # output!
